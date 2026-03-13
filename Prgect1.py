e238640522e8c4f437e768e284a960d5fb72ef19
gideonclemmons-creator <gideonclemmons@seattleacademy.org>
2026-03-13 21:50:14 +0000

    Project 1

--- Prgect1.py
+++ Prgect1.py
@@ -16,17 +16,17 @@ print("lucky you you get to")
 input()
 print("pick a number between 1-2 but dont type it")
 input()
-print("I bet it was 1 or 2 cool right i just guessed the number")
+print("I bet it was 1 or 2 cool right i just guessed " \
+"the number")
 input()
 answer1 = input("alright new game If 1+2=4 what dose 2+2= type your answer")
 if answer1 == "67":
     answer2 = input("Wow you must have cheated or something good job")
-if answer1 == "":
-    answer3 = input("wrong")
-print("no try again")
+else:
+    answer3 = input("wrong try again")
+    if answer3 == "67":
+        print("Wow you must have cheated or something good job")
+    else:
+        print("No not close but I will give it to you the answer was 67")
 input()
-print("no but closer try again")
-input()
-print(r"Hah you weren't close at all but I will give it to you the answer was 67 you should have gotten it the first time :)")
-input()
-print(f"thanks for talking to me {name} :)")
+print(fr"thanks for talking to me {name} :)")
